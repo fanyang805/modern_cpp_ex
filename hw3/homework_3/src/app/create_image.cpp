a278e5336f366c344a066ecd1cd135b641bbcdc9
fan <phil8905@gmail.com>
2020-09-23 11:14:13 -0400

    hw4 created

--- hw3/homework_3/src/app/create_image.cpp
+++ hw3/homework_3/src/app/create_image.cpp
@@ -1,13 +1,13 @@
 #include "image_browser.hpp"
-#include <chrono>
+// #include <chrono>
 #include <cstdlib>
 #include <ctime>
 #include <string>
-#include <thread>
+// #include <thread>
 
 int rand_num(int min, int max) {
-  std::chrono::seconds dura(1);
-  std::this_thread::sleep_for(dura);
+  // std::chrono::seconds dura(1);
+  // std::this_thread::sleep_for(dura);
   std::srand(static_cast<unsigned int>(std::time(nullptr)));
   static constexpr double fraction{1.0 / (RAND_MAX + 1.0)};
   return min + (max - min + 1) * (std::rand() * fraction);
