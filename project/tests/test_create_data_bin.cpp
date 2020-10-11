36bc6d8eb9bd3b91cc8bb45b175dd65090b0d80a
Fan Yang <phil8905@gmail.com>
2020-10-11 16:38:32 -0400

    test file updated

--- project/tests/test_create_data_bin.cpp
+++ project/tests/test_create_data_bin.cpp
@@ -10,29 +10,28 @@
 namespace fs = std::filesystem;
 
 int main() {
-  // Create the squence to read img file and generate binary file
-  const std::string &img_path{"tests/data/images/"};
+  const std::string &bin_path{"data/bin/"};
+  const std::string &bin_extension{".bin"};
+  std::vector<std::string> bin_sequence =
+      bovw::get_sequence(bin_path, bin_extension, 0, 691);
+
+  std::cout << "Start to read binary files.\n";
+  std::vector<cv::Mat> vec_des = bovw::serialization::sifts::LoadDataset(
+      "tests/data/bin/", bin_sequence, 700);
+
+  std::cout << "Start to compare SIFT descripters from original .png files and "
+               ".bin files\n";
+  int idx = 0;
+  const std::string &img_path{"data/images/"};
   const std::string &extension{".png"};
   std::vector<std::string> img_sequence =
-      bovw::get_sequence(img_path, extension, 0, 9);
-  // Extract SIFT descripters from .png images and convert descripters to the
-  // .bin binary files
-  std::vector<std::string> bin_sequence =
-      bovw::serialization::sifts::ConvertDataset(img_path, img_sequence, 12);
-
-  // Convert binary files to the vector of SIFT descripters
-  std::vector<cv::Mat> vec_des = bovw::serialization::sifts::LoadDataset(
-      "tests/data/bin/", bin_sequence, 12);
-
-  // Compare the descripters extracted from the binaries to the original
-  // descripters in the .png images.
-  int idx = 0;
+      bovw::get_sequence(img_path, extension, 0, 691);
   for (const std::string &filename : img_sequence) {
     cv::Mat descriptors = bovw::compute_sifts(filename);
+    std::cout << "Are desicripters of " << fs::path(filename).stem()
+              << " same in .png and .bin files? ";
     std::cout << std::boolalpha
               << bovw::mat_are_equal<float>(descriptors, vec_des[idx++])
               << std::endl;
   }
-
-  return 0;
 }
